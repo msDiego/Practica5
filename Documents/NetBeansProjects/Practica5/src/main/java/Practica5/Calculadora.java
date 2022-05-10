65636c60f1fadd442051b621583727861fffe7b4
msDiego <dmorrosilva@cifpfbmoll.eu>
2022-05-10 17:49:29 +0200

    Primera versión interfaz ICalculadora implementada

---
+++ Documents/NetBeansProjects/Practica5/src/main/java/Practica5/Calculadora.java
@@ -0,0 +1,34 @@
+package Practica5;
+
+
+public class Calculadora implements ICalculadora {
+
+    public static void main(String[] args) {
+        
+    }
+
+    @Override
+    public double sumar(double x, double y) {
+        double resultado = 0;
+        return resultado;
+    }
+
+    @Override
+    public double restar(double x, double y) {
+        double resultado = 0;
+        return resultado;
+    }
+
+    @Override
+    public double multiplicar(double x, double y) {
+        double resultado = 0;
+        return resultado;
+    }
+
+    @Override
+    public double dividir(double x, double y) {
+        double resultado = 0;
+        return resultado;
+    }
+
+}
---
+++ Documents/NetBeansProjects/Practica5/src/main/java/Practica5/ICalculadora.java
@@ -0,0 +1,12 @@
+
+package Practica5;
+
+
+public interface ICalculadora {
+    
+    public double sumar(double x, double y);
+    public double restar(double x, double y);
+    public double multiplicar(double x, double y);
+    public double dividir(double x, double y);
+    
+}
